ac7556d2663996a4db20d2a4d263ed83fbc97043
AthensCort <athens.cort@gmail.com>
2025-04-03 12:38:22 -0600

    Actualizacion para handler, modificacion controller expotación

--- controllers/studentControler.ts
+++ controllers/studentControler.ts
@@ -1,23 +1,33 @@
 import StudentService from "../db/studentModel";
 
-export const obtenerAlumnos = () => {
-  const estudiantes = StudentService.getAllStudents();
+class StudentController {
+  private studentService;
 
-  return estudiantes.map((alumno) => {
-    let mensaje = "";
+  constructor(studentService: typeof StudentService) {
+    this.studentService = studentService;
+  }
 
-    if (alumno.calificacion > 90) {
-      mensaje = alumno.adeudos
-        ? "Tenías honores pero tienes un adeudo."
-        : "Te graduaste con honores.";
-    } else if (alumno.calificacion >= 70) {
-      mensaje = "Pasaste muy apenas.";
-    } else {
-      mensaje = alumno.adeudos
-        ? "Expulsado."
-        : "Como no tienes adeudo, tienes derecho a un examen de recuperación.";
-    }
+  getAllStudents() {
+    const estudiantes = this.studentService.getAllStudents();
 
-    return { ...alumno, mensaje };
-  });
-};
+    return estudiantes.map((alumno) => {
+      let mensaje = "";
+
+      if (alumno.calificacion > 90) {
+        mensaje = alumno.adeudos
+          ? "Tenías honores pero tienes un adeudo."
+          : "Te graduaste con honores.";
+      } else if (alumno.calificacion >= 70) {
+        mensaje = "Pasaste muy apenas.";
+      } else {
+        mensaje = alumno.adeudos
+          ? "Expulsado."
+          : "Como no tienes adeudo, tienes derecho a un examen de recuperación.";
+      }
+
+      return { ...alumno, mensaje };
+    });
+  }
+}
+
+export default StudentController;
