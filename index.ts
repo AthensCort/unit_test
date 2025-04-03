8868da17480dce7a3da1d6ee151c0c1d39b6ae15
AthensCort <athens.cort@gmail.com>
2025-04-03 12:42:17 -0600

    server apply

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import studentRoutes from "./routes/studentRoutes";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use("/api", studentRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+
+//general index para els ervidor :P
