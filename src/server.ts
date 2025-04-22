c8dd9d6f45973b282cabbb1b09f43e92b7057583
AthensCort <athens.cort@gmail.com>
2025-04-22 11:36:27 -0600

    Identificacion-Mensaje

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,16 @@ const PORT = 3000;
 //esa es otra aprte
 app.use(express.json());
 app.use("/api", studentRoutes);
-
+//api/students
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+
+//CAROLINA DE JESUS ORTEGA CEPEDA A01282386
+//ACTIVIDAD DE ESTUDIANTES SOBRE BASE DE DATOS /SI PASAN O NO
+//ACTIVIDAD EN CLASE :P (3 DE ABRIL)
 
 
 //general index para els ervidor :P
